ab62f6c4571d80d7544552276839ffe8a6feba51
RedAzaki <107935485+RedAzaki@users.noreply.github.com>
2023-04-02 16:48:38 +0500

    update

--- XYZ/src/test/java/com/globalsqa/FirstTest.java
+++ XYZ/src/test/java/com/globalsqa/FirstTest.java
@@ -1,32 +1,57 @@
 package com.globalsqa;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class FirstTest extends WebDriverSettings{
+public class FirstTest extends WebDriverSettings {
 
     @Test
-    public void firstTest() {
+     public void firstTest() {
 
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
         String title = driver.getTitle();
         Assert.assertEquals("XYZ Bank", title);
 
-        WebElement button = driver.findElement(By.xpath("(//button[@class='btn btn-primary btn-lg'])[1]"));
-        button.click();
+        //Заголовок страницы
+        boolean header = driver.findElement(By.cssSelector("strong[class='mainHeading']")).isDisplayed();
+        //кнопка Домой
+        WebElement btnHome = driver.findElement(By.cssSelector("button[class='btn home']"));
+        //кнопка Customer Login
+        WebElement btnCustLog = driver.findElement(By.cssSelector("button[ng-click='customer()']"));
+        //кнопка Bank Manager Login
+        WebElement btnManLog = driver.findElement(By.cssSelector("button[ng-click='manager()']"));
+        btnManLog.click();
 
-        WebElement inputLogin = driver.findElement(By.xpath("//div[@class='form-group']"));
-        inputLogin.click();
+        //Проверка кнопок в Bank Manager Login
+        WebElement btnAddCust = driver.findElement(By.cssSelector("button[ng-click='addCust()']"));
+        WebElement btnOpenAcc = driver.findElement(By.cssSelector("button[ng-click='openAccount()']"));
+        WebElement btnCust = driver.findElement(By.cssSelector("button[ng-click='showCust()']"));
 
-        WebElement choisName = driver.findElement(By.xpath("//option[@value='1']"));
-        choisName.click();
+        //Добавляем пользователя
+        btnAddCust.click();
+        //Поле Имя
+        WebElement inpFname = driver.findElement(By.cssSelector("input[placeholder='First Name']"));
+        inpFname.sendKeys("Bear");//Сюда надо добавить генератор слов
+        //Поле Фамилия
+        WebElement inpLname = driver.findElement(By.cssSelector("input[placeholder='Last Name']"));
+        inpLname.sendKeys("Pupkin");
+        //Поле Пост код
+        WebElement inpPosCod = driver.findElement(By.cssSelector("input[placeholder='Post Code']"));
+        inpPosCod.sendKeys("78DDC9");
+
+        //Кнопка добавить
+        WebElement btnAdCust = driver.findElement(By.cssSelector("button[class='btn btn-default']"));
+         btnAdCust.click();
+
+
+//
+
+
+
+
+
 
 
 
